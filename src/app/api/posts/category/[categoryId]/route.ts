569f17279cf531b10ac1fed1c230ff0150c08c21
LitovicMateo <litovicm@gmail.com>
2024-04-16 15:57:56 +0200

    category api try-catch

--- src/app/api/posts/category/[categoryId]/route.ts
+++ src/app/api/posts/category/[categoryId]/route.ts
@@ -3,12 +3,12 @@ import { NextResponse } from "next/server";
 const API_URL = process.env.NEXT_PUBLIC_WORDPRESS_API_ENDPOINT as string;
 
 export async function GET(req: Request, context: any) {
-  const { params }: {params: {categoryId: string}} = context;
-  console.log(params);
-  console.log(req);
-  
+	const { params }: { params: { categoryId: string } } = context;
+	console.log(params);
+	console.log(req);
 
-  const query = `{
+	let data;
+	const query = `{
     posts(where: {categoryName: "${params.categoryId}"}) 
     {
       nodes {
@@ -36,17 +36,19 @@ export async function GET(req: Request, context: any) {
   }
   `;
 
-  const res = await fetch(API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: query,
-    }),
-  });
-  const data = await res.json();
-  console.log(data);
+	try {
+		const res = await fetch(API_URL, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				query: query,
+			}),
+		});
+		data = await res.json();
+	} catch (error) {}
+	console.log(data);
 
-  return NextResponse.json(data);
+	return NextResponse.json(data);
 }

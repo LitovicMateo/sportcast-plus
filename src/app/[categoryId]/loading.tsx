2f2110639346c5a5676c42b9fb129da9657188d0
LitovicMateo <litovicm@gmail.com>
2024-04-17 13:59:34 +0200

    loading screen

--- src/app/[categoryId]/loading.tsx
+++ src/app/[categoryId]/loading.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
 
 
 export default function CategoryLoading() {
     // Or a custom loading skeleton component
-    return <p>Loading...</p>
+    return <div className="w-full h-svh flex justify-center items-center">
+       <Image src={"/logo.png"} height={50} width={50} alt="logo" className="aspect-square max-w-[50px]" />
+    </div>
   }

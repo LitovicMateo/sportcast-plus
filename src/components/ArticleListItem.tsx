f62b2aca2cfff1e2241f4baac697d755f88ab357
LitovicMateo <litovicm@gmail.com>
2024-05-24 16:52:19 +0200

    fix

--- src/components/ArticleListItem.tsx
+++ src/components/ArticleListItem.tsx
@@ -33,7 +33,7 @@ const ArticleListItem: React.FC<ArticleListItemProps> = ({ post }) => {
           <BreakLine />
         </div>
         <span className={`text-[12px] md:text-[16px] text-[#B9B9B9] uppercase ${mplus.className}`}>
-          AUTOR: {post.author.node.name} | {date}
+          AUTOR:  | {date}
         </span>
         <div
           className={`text-wrap text-[18px] hidden md:block leading-7 line-clamp-5 ${amiri.className}`}

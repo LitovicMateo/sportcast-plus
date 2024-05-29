f26c9215ef84a90cd45374d11dce6090e5872ebd
LitovicMateo <litovicm@gmail.com>
2024-05-29 22:14:54 +0200

    featured image fix

--- src/components/posts/featured-image.tsx
+++ src/components/posts/featured-image.tsx
@@ -8,8 +8,8 @@ type FeaturedImageProps = {
 
 const FeaturedImage: React.FC<FeaturedImageProps> = ({ imageUrl, slug }) => {
   return (
-    <div className="w-full h-full max-h-[450px] overflow-hidden flex justify-center items-center ">
-      <Image className="aspect-auto w-full" src={imageUrl} width={1000} height={300} alt={slug} />
+    <div className="relative w-full aspect-[16/10] max-w-[1200px] mx-auto overflow-hidden flex justify-center items-center ">
+      <Image className="aspect-auto w-full" src={imageUrl} fill objectFit="contain" objectPosition="top" alt={slug} />
     </div>
   );
 };

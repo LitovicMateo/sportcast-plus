ceb5df1445c1ed4a1603753137c651a1c353a04b
LitovicMateo <litovicm@gmail.com>
2024-05-31 16:34:18 +0200

    test preview

--- src/app/(app)/preview/[slug]/page.tsx
+++ src/app/(app)/preview/[slug]/page.tsx
@@ -1,7 +1,56 @@
+import { gql } from '@apollo/client';
 import { WordPressTemplate } from '@faustwp/core';
+import { getAuthClient, getClient } from '@faustwp/experimental-app-router';
 
-export default function Preview({ params}: { params: {slug: string}}) {
-  console.log(params.slug);
+function hasPreviewProps(props: any) {
+  return props?.searchParams?.preview === 'true' && !!props?.searchParams?.p;
+}
+
+type Props = {
+  props: {
+    params: {
+      slug: string
+    }
+  }
+}
+
+
+export default async function Preview({ props }: Props) {
+  console.log(props.params.slug);
+
+  const isPreview = hasPreviewProps(props)
+
+  const id = props.params.slug;
+
+  let client = isPreview ? await getAuthClient() : await getClient();
+
+  const { data } = await client!.query({
+    query: gql`
+      query GetContentNode(
+        $id: ID!
+        $idType: ContentNodeIdTypeEnum!
+        $asPreview: Boolean!
+      ) {
+        contentNode(id: $id, idType: $idType, asPreview: $asPreview) {
+          ... on NodeWithTitle {
+            title
+          }
+          ... on NodeWithContentEditor {
+            content
+          }
+          date
+        }
+      }
+    `,
+    variables: {
+      id,
+      idType: isPreview ? 'DATABASE_ID' : 'URI',
+      asPreview: isPreview,
+    },
+  });
+
+  console.log(data);
   
-  return <div>Preview page for {params.slug}</div>;
+  
+  return <div>Preview page for {props.params.slug}</div>;
 }

7dd0fa6a39bf5e11bff32ad29d86bf1e196a721c
LitovicMateo <132395067+LitovicMateo@users.noreply.github.com>
2024-06-05 17:49:14 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     env: {
         title: "SportCastPlus",
         NEXT_PUBLIC_WORDPRESS_URL:"https://lime-panther-317414.hostingersite.com",
-        NEXT_PUBLIC_URL:"https://sportcast-plus-git-dev-litovicmateos-projects.vercel.app/",
+        NEXT_PUBLIC_URL:"https://sportcastplus.com/",
         FAUST_SECRET_KEY:"72dbd92e-14d3-4d4e-b143-639c6756df40",
         NEXT_PUBLIC_WORDPRESS_API_ENDPOINT: "https://lime-panther-317414.hostingersite.com/graphql",
         NEXT_PUBLIC_LOCAL_API_ENDPOINT: "http://localhost:3000",
